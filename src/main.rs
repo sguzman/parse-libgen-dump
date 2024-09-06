462b9f6e533858193cadb742896c95882ed90c24
Salvador Guzman <guzmansalv@gmail.com>
2024-09-06 11:18:46 -0700

    ignore cache

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
+use cached::proc_macro::io_cached;
+use cached::DiskCache;
 use env_logger::Env;
 use log::info;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use thiserror::Error;
+
+#[derive(Debug, PartialEq, Clone, Error)]
+enum CacheError {
+    #[error("error with disk cache `{0}`")]
+    DiskError(String),
+}
+
+/// Cache the results of a function on disk.
+/// Cache files will be stored under the system cache dir
+/// unless otherwise specified with `disk_dir` or the `create` argument.
+/// A `map_error` closure must be specified to convert any
+/// disk cache errors into the same type of error returned
+/// by your function. All `io_cached` functions must return `Result`s.
+#[io_cached(
+    map_error = r##"|e| CacheError::DiskError(format!("{:?}", e))"##,
+    disk = true,
+    sync_to_disk_on_cache_change = true,
+    disk_dir = ".cache"
+)]
+fn cached_sleep_secs(secs: u64) -> Result<String, CacheError> {
+    std::thread::sleep(std::time::Duration::from_secs(secs));
+    Ok(secs.to_string())
+}
 
 const INPUT_FILE: &str = "resources/data.sql";
 
@@ -11,6 +37,9 @@ fn main() -> std::io::Result<()> {
 
     let input_file = Path::new(INPUT_FILE);
     info!("Processing file: {}", input_file.display());
+
+    let result = cached_sleep_secs(5);
+    info!("Result: {:?}", result);
 
     info!("SQL file processing completed successfully");
     Ok(())
